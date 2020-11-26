cb37bed5730613ac06c2843170565f77d712ca8f
pgraffigna <69170202+pgraffigna@users.noreply.github.com>
2020-11-26 17:40:20 -0300

    Actualización crear_usuarios.sh

--- crear_usuarios.sh
+++ crear_usuarios.sh
@@ -47,38 +47,35 @@ read -p "Ingresa el nombre del usuario, el password y el n° de patrimonio separ
 
 # validando que el array contenga "solo" 3 elementos
 	if [ "${#DATOS[@]}" -ne 3  ]; then
-	   echo -e "\n${redColour} [++] Los datos cargados no son suficientes, cargar correctamente"
-       exit 0
+	   	echo -e "\n${redColour} [++] Los datos cargados no son suficientes, cargar correctamente"
+           	exit 0
 	else
-
 		egrep "^${DATOS[0]}" /etc/passwd >/dev/null
 
-  		if [ $? -eq 0 ]; then
-		echo -e "\n${redColour} [++] El usuario ${DATOS[0]} ya existe, saliendo del programa!!! ${endColour}"
-		exit 0
-
+		if [ $? -eq 0 ]; then
+			echo -e "\n${redColour} [++] El usuario ${DATOS[0]} ya existe, saliendo del programa!!! ${endColour}"
+			exit 0
 		else
-       	pass=$(perl -e 'print crypt($ARGV[0], "PASSWORD")' "${DATOS[1]}")
-       	useradd -s /bin/bash -m -p "$pass" "${DATOS[0]}"
+       	        	pass=$(perl -e 'print crypt($ARGV[0], "PASSWORD")' "${DATOS[1]}")
+                	useradd -s /bin/bash -m -p "$pass" "${DATOS[0]}"
 
-		[ $? -eq 0 ]
-      	cp -Rfa /home/vagrant/\. /home/"${DATOS[0]}"
-		chown -R "${DATOS[0]}"":""${DATOS[0]}" /home/"${DATOS[0]}"/
-   		echo -e "\n${greenColour} El usuario ${DATOS[0]} fue añadido con exito! ${endColour}"
+			[ $? -eq 0 ]
+      	        	cp -Rfa /home/vagrant/\. /home/"${DATOS[0]}"
+			chown -R "${DATOS[0]}"":""${DATOS[0]}" /home/"${DATOS[0]}"/
+   			echo -e "\n${greenColour} El usuario ${DATOS[0]} fue añadido con exito! ${endColour}"
 
-		hostname="alv2""-""${DATOS[2]}"
+			hostname="alv2""-""${DATOS[2]}"
 
-		echo -e "\n${yellowColour} El nombre del equipo es $hostname ${endColour}"
+			echo -e "\n${yellowColour} El nombre del equipo es $hostname ${endColour}"
+			echo "$hostname" > /etc/hostname
+			echo "	127.0.0.1 localhost
+           			127.0.1.1 $hostname" > /etc/hosts
 
-		echo "$hostname" > /etc/hostname
-		echo "	127.0.0.1 localhost
-           		127.0.1.1 $hostname" > /etc/hosts
-
-		echo "DrivePathC=/home/${DATOS[0]}/Escritorio/" >> /home/"${DATOS[0]}"/.ICAClient/wfclient.ini 
-       	echo -e "\n${greenColour} Todos los procesos terminaron correctamente :) ${endColour}"
+			echo "DrivePathC=/home/${DATOS[0]}/Escritorio/" >> /home/"${DATOS[0]}"/.ICAClient/wfclient.ini 
+           		echo -e "\n${greenColour} Todos los procesos terminaron correctamente :) ${endColour}"
 		fi
 	fi
 else
-		echo -e "${redColour} Correr con SUDO!!! ${endColour}"
-		exit 0
+	echo -e "${redColour} Correr con SUDO!!! ${endColour}"
+	exit 0
 fi
