8248fa739ec802f930c68b7deba4168a290f63f6
pgraffigna <pabloggraffigna@gmail.com>
2022-04-01 10:42:54 -0300

    modificacion scripts

--- zsh_config/01-plugins_fonts_themes_zsh.sh
+++ zsh_config/01-plugins_fonts_themes_zsh.sh
@@ -10,36 +10,35 @@ fin="\033[0m\e[0m"
 # Ctrl-C
 trap ctrl_c INT
 function ctrl_c(){
-        echo -e "\n${rojo}Programa Terminado por el usuario ${fin}"
-        exit 0
+echo -e "\n${rojo}Programa Terminado por el usuario ${fin}"
+exit 0
 }
 
-# backup .zshrc original
-cp -r ~/.zshrc ~/.zshrc-backup
-echo -e "${verde}Realizado backup de .zshrc en .zshrc-backup ${fin}\n"
-
-# copiando archivos 
-cp ~/zsh_config/p10k.zsh ~/.p10k.zsh
-cp ~/zsh_config/zshrc ~/.zshrc
+echo -e "${verde}Realizando backup de .zshrc en .zshrc-backup ${fin}\n"
+cp ~/.zshrc ~/.zshrc-backup
 
 # plugins
-git clone --quiet --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions &>/dev/null
-git clone --quiet --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting &>/dev/null
-git clone --quiet --depth=1 https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions &>/dev/null
+echo -e "${amarillo}Instalando Plugins ${fin}\n"
+git clone --quiet https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &>/dev/null
+git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &>/dev/null
+git clone --quiet https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions &>/dev/null
 
 # fuentes
 echo -e "${amarillo}Instalando Nerd Fonts ${fin}\n"
 wget -q --show-progress -N --progress=bar:force 2>&1 https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf -P ~/.fonts/
 wget -q --show-progress -N --progress=bar:force 2>&1 https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf -P ~/.fonts/
 wget -q --show-progress -N --progress=bar:force 2>&1 https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete.ttf -P ~/.fonts/
-fc-cache -fv ~/.fonts 2>/dev/null
+fc-cache -fv ~/.fonts 
 
 # temas
-git clone --quiet --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k &>/dev/null
+echo -e "${amarillo}Instalando powerlevel10k ${fin}\n"
+git clone --quiet https://github.com/romkatv/powerlevel10k ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k &>/dev/null
 
-# configurar como default
-chsh -s $(which zsh)
+echo -e "${amarillo}Configurando el tema powerlevel10k ${fin}\n" 
+sed -i 's#ZSH_THEME="robbyrussell"#ZSH_THEME="powerlevel10k/powerlevel10k"#' .zshrc
 
-echo -e "${verde}[**] Cerrar y Re-Abrir la sesion para que tome los cambios [**] ${fin}"
+echo -e "${verde}[**] 1 - Configurar la fuente en la terminal [**] ${fin}"
+echo -e "${verde}[**] 2 - cerrar/abrir la terminal configurar p10k [**] ${fin}"
 
 
+                
